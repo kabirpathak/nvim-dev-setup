1ff53fd4111d3e5a98983b6ece1abda82e7e1e57
kabirpathak <kabirpathak99@gmail.com>
2024-10-04 22:51:04 +0530

    nvim-tree focuses on currently open file.

--- lua/kabirpathak/plugins/nvim-tree.lua
+++ lua/kabirpathak/plugins/nvim-tree.lua
@@ -8,7 +8,12 @@ vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
 nvimtree.setup({
+  update_focused_file = {
+    enable = true,        -- Automatically focus the current file in the tree
+    update_cwd = true,    -- Optionally update the current working directory
+  },
   actions = {
     open_file = {
       window_picker = {
